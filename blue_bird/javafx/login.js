5299ac16e4545768533b7cf863d2c32f0579cac8
starxg <763860000@qq.com>
2018-11-12 00:00:56 +0800

    Update login.js

--- blue_bird/javafx/login.js
+++ blue_bird/javafx/login.js
@@ -1,4 +1,4 @@
-$('section').css('background', 'none').find('.text-login>h1').remove().end().find('.login').css({'float': 'none'});
+$('section').css('background', 'none').find('.text-login>h1').remove().end().find('.login').css({'float': 'none','margin':':20px auto'});
 $('header').remove();
 $('footer').remove();
 $('.gray').click();
